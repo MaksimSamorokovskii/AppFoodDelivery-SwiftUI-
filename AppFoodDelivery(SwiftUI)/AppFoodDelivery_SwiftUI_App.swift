b8832e0f919384ef7017a66a8fdce10f18214541
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2023-11-17 12:15:18 +0400

    Initial commit

---
+++ AppFoodDelivery(SwiftUI)/AppFoodDelivery_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  AppFoodDelivery_SwiftUI_App.swift
+//  AppFoodDelivery(SwiftUI)
+//
+//  Created by Максим Самороковский on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppFoodDelivery_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
